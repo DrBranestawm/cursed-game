b8e6269e511a4cd3d5dace22afa1347147f87cba
Lilly #85723 <115511728+DrBranestawm@users.noreply.github.com>
2023-07-27 21:46:46 +0200

    Update sl.config.js

--- cursed/js/sl.config.js
+++ cursed/js/sl.config.js
@@ -330,7 +330,7 @@ var playerTemplates = [
 "hair length":0,
 "physique":0,
 "ass size":0,},
-{"name":"Jenny","last name":"Rose","age":20,"height":180,"gender":1,"hair color":"Blonde","eye color":"Blue",
+{"name":"Jenny","last name":"Rose","age":20,"height":180,"gender":0,"hair color":"Blonde","eye color":"Blue",
 "desc":"", //hidden sandbox character, do not remove
 "gear":[],
 "strength":1,
